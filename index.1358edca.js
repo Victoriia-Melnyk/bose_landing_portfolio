0b073e4394e111aaef9c826cd0643ef735c329e2
Viktoriia Melnyk <melnychka1993@gmail.com>
2024-12-06 10:40:36 +0000

    make build

---
+++ index.1358edca.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.getElementById("myForm");e.addEventListener("submit",function(n){n.preventDefault(),e.reset()});
+//# sourceMappingURL=index.1358edca.js.map
